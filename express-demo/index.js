514474b58b82c926369cb611d3fc45ea1de93efe
peterkinuthia <peterchege442@gmail.com>
2020-01-07 00:46:05 +0300

    correct environment variable

--- express-demo/index.js
+++ express-demo/index.js
@@ -19,5 +19,5 @@ app.get('/api/courses', (req, res)=>{
 // app.put();
 
 // ENVIRONMENT VARIABLE
-const port = process.env.app || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}...`));
