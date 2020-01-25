10c55795b6eba4ec607f6f5f49d7ae1ea5f707e8
peterkinuthia <peterchege442@gmail.com>
2020-01-25 21:12:17 +0300

    separating middle wares

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,14 +1,12 @@
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
 //Creating a custom middleware 
-app.use(function (req, res, next){
-    console.log('Logging...');
-    next();
-});
+app.use(logger);
 
 app.use(function (req, res, next) {
     console.log('Authenticating...');
