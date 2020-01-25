1548089feb79ae0f6a215218c2233df7f2e8c516
peterkinuthia <peterchege442@gmail.com>
2020-01-25 21:22:17 +0300

    urlencoded middleware

--- express-demo/index.js
+++ express-demo/index.js
@@ -5,10 +5,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Creating a custom middleware 
 app.use(logger);
-
 app.use(authenticator);
 
 const courses = [
