84a4f25748ad35f0e3cf9d79c9faf9e61982ac90
peterkinuthia <peterchege442@gmail.com>
2020-01-27 23:24:36 +0300

    creating a timeout request made to the database

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,14 @@
 console.log('Before');
-setTimeout(()=>{
-    console.log('Reading a user from a database...')
-}, 2000);
-console.log('After')
+const user = getUser(1);
+console.log('After');
+
+
+
+function getUser(id) {
+    setTimeout(() => {
+        console.log('Reading a user from a database...');
+        return {id:id, gitHunUsername: 'peter'};
+    }, 2000);
+
+    return 1;
+}
