b93f49a18edc93125676f9f22f3f7def6a52379c
peterkinuthia <peterchege442@gmail.com>
2020-02-06 22:51:10 +0300

    parralel Promises

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,6 +1,23 @@
 
-const p = Promise.resolve({id: 1});
-p.then(result => console.log(result));
+// const p = Promise.resolve({id: 1});
+// p.then(result => console.log(result));
 
-const j = Promise.reject(new Error('This is the reason for rejection'));
-j.catch(error => console.log(error));
+// const j = Promise.reject(new Error('This is the reason for rejection'));
+// j.catch(error => console.log(error));  
+
+const p1 = new Promise ((resolve) =>{
+    setTimeout(() => {
+        console.log('Async operation 1.....');
+        resolve(1);
+    }, 2000); 
+});
+
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async operation 2.....');
+        resolve(2);
+    }, 2000);
+});
+
+Promise.all([p1, p2])
+    .then(result => console.log(result)); 
