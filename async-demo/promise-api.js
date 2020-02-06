0e3f360c75daa519fea3799f0e6fefbfeedff8bd
peterkinuthia <peterchege442@gmail.com>
2020-02-06 22:31:39 +0300

    complete apia

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,6 +1,6 @@
 
-// const p = Promise.resolve({id: 1});
-// p.then(result => console.log(result));
+const p = Promise.resolve({id: 1});
+p.then(result => console.log(result));
 
 const j = Promise.reject(new Error('This is the reason for rejection'));
 j.catch(error => console.log(error));
