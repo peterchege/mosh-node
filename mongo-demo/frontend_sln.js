a2a09897d58b532bce351569c735a80653d30610
peterkinuthia <peterchege442@gmail.com>
2020-02-12 01:18:16 +0300

    solution to the frontend

---
+++ mongo-demo/frontend_sln.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-exercises')
+
+const courses = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: Date,
+    isPublished: Boolean,
+    price: Number
+});
+
+const Course = mongoose.model('Course', courses)
+
+async function getCourses(){
+    return await Course
+    .find()
+    .and({ tags: 'frontend' }, { tags: 'backend'})
+    .sort({price: -1})
+    .select({name: 1, author: 1});
+}
+
+async function run(){
+    const courses = await getCourses();
+    console.log(courses);
+}
+
+run();
