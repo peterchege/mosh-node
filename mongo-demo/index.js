4798f487c829ea8cd67cb6bf81dcac723d4527c9
peterkinuthia <peterchege442@gmail.com>
2020-02-11 23:09:50 +0300

    comparison operators in mongo db using gte and lte

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -31,8 +31,19 @@ async function createCourse() {
 
 // Querying from Mongodb
 async function getCourses() {
+
+// comparison operators
+// eq (equal)
+// ne (not equal)
+// gt (greater than)
+// gte (greater than or equal to)
+// lt (less than)
+// lte (less than or equal to)
+// in 
+// nin (not in)
     const courses = await Course
-    .find({ author: 'Peter', isPublished: true})
+    // .find({ author: 'Peter', isPublished: true})
+    .find({ price: { $gte: 10, $lte: 20 } })
     .limit(10)
     .sort({ name: 1 })
     .select({ name : 1, tags: 1 });
