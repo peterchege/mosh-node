e0d31e1ad9d6d391972dfcae0179032a05fd80ab
peterkinuthia <peterchege442@gmail.com>
2020-02-12 01:30:24 +0300

    optn 2

--- mongo-demo/frontend_sln.js
+++ mongo-demo/frontend_sln.js
@@ -16,8 +16,11 @@ const Course = mongoose.model('Course', courses)
 async function getCourses(){
     return await Course
     .find({isPublished: true, tags: {$in : ['frontend','backend']} })
+    // .or([{tags: 'frontend'}, {tags: 'backend'}]) option 2 using logic operators
     .sort({price: -1})
+    // .sort('-price') opt2 using strings
     .select({name: 1, author: 1, price: 1});
+    // .select('name author price') opt2 using strings
 }
 
 async function run(){
