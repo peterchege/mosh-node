11bdb0395301c84b1a94c68be14c8badd4221f1d
peterkinuthia <peterchege442@gmail.com>
2020-02-11 20:35:07 +0300

    try to resolve the quiz

---
+++ async-demo/try.js
@@ -0,0 +1,50 @@
+async function NotifyCustomer() {
+    try {
+        const customer = await getCustomer(1);
+        console.log('Customer', customer);
+        if (customer.isGold) {
+            const movies = await getTopMovies();
+            console.log('Top movies: ', movies);
+            const email = await sendEmail();
+            console.log('Email sent...')
+        }
+    }
+
+    catch (err) {
+        console.log(err);
+    }
+
+}
+
+NotifyCustomer();
+
+
+function getCustomer(id){
+    return new Promise ((resolve, reject) => {
+         setTimeout(() => {
+             resolve({
+                 id: 1,
+                 name: 'Mosh Hamedani',
+                 isGold: true,
+                 email: 'email'
+             });
+         }, 4000);
+    });
+}
+
+function getTopMovies() {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            resolve(['movie1', 'movie2']);
+        }, 4000);
+    });
+    
+}
+
+function sendEmail(email, movies) {
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            resolve();
+        }, 4000);
+    });
+}
