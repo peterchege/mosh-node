835df1e42344a5a455033f404bf468a7a186f7a5
peterkinuthia <peterchege442@gmail.com>
2020-02-11 23:38:02 +0300

    regular Expressions

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -47,13 +47,28 @@ async function getCourses() {
 // and
     const courses = await Course
     // .find({ author: 'Peter', isPublished: true})
+
     // Comparison operators examples
     // .find({ price: { $gte: 10, $lte: 20 } })
     // .find({ price: { $in: [10, 50, 20] } })
 
     // Logical operators example
-    .find()
-    .or([ { author: 'Peter' }, {isPublished: true} ])
+    // .find()
+    // .or([ { author: 'Peter' }, {isPublished: true} ])
+
+    // Regular Expressions template
+    .find({ author: /pattern/})
+
+    // starting with Peter
+    .find({ author: /^Peter/ })
+
+    //Ending with Peter 
+    .find({ author: /Peter$/ }) //case sensitive
+    .find({ author: /Peter$/i })
+
+    // Contains peter
+    .find({ author: /.*peter.*/i})
+
     .limit(10)
     .sort({ name: 1 })
     .select({ name : 1, tags: 1 });
