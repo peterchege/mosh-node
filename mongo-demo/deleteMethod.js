b5b6c5a9f4fef0a197741778ac49cd1ff94a5e3d
peterkinuthia <peterchege442@gmail.com>
2020-02-14 01:15:53 +0300

    implementing delete method in courses

---
+++ mongo-demo/deleteMethod.js
@@ -0,0 +1,25 @@
+const mongoose = require ('mongoose');
+
+// connection to Mongodb
+mongoose.connect('mongodb://localhost/playground')
+.then( () => console.log('Connected to MongoDB....'))
+.catch(err => console.err("Could not connect to MongoDB...", err))
+
+// schema definition
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+
+//update a document and return it
+async function deletCourse(id) {
+    const result = await Course.deleteOne({_id: id});
+    console.log(result);
+}
+deletCourse('5e45c19b826f81269c3c4b57');
