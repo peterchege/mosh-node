18280d5dbb202fcb5ccf3768300ff3d245a28c17
peterkinuthia <peterchege442@gmail.com>
2020-02-16 00:51:04 +0300

    update fn with mongodb

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -32,24 +32,24 @@ router.get('/', async (req, res) => {
   res.send(genres);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { error } = validateGenre(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
   let genre = new Genres({ name: req.body.name });
-  genre = await genre.save(genre);
-  
+  genre = await genre.save();
+
   res.send(genre);
 });
 
-router.put('/:id', (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-
+router.put('/:id', async (req, res) => {
   const { error } = validateGenre(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
-  
-  genre.name = req.body.name; 
+
+  const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name}, {new: true} );
+
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
   res.send(genre);
 });
 
