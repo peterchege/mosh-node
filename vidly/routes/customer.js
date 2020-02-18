8fce7280814a631ecef19b8259ebcaaef664cb8c
peterkinuthia <peterchege442@gmail.com>
2020-02-18 22:31:21 +0300

    data model completd

--- vidly/routes/customer.js
+++ vidly/routes/customer.js
@@ -7,7 +7,18 @@ const router =  express.Router();
 const Customer = new mongoose.model(mongoose.Schema({
     name: {
         type: String,
-        
+        required: true,
+        minlenght: 5,
+        maxlength: 50
+    },
+    phone: {
+        type: Number,
+        required: true,
+        minlenght: 12
+    },
+    isGold:{
+        type: Boolean,
+        required: true
     }
 
 }));
