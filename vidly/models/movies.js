911601dfad109c675d2e5bc62bfb3a64dd6d429c
peterkinuthia <peterchege442@gmail.com>
2020-03-07 13:42:17 +0300

    compelete

--- vidly/models/movies.js
+++ vidly/models/movies.js
@@ -4,10 +4,10 @@ const mongoose = require ('mongoose');
 
 const genreSchema = new mongoose.Schema({
     name: {
-                type: String,
-                required: true,
-                minlength: 5,
-                maxlength: 50
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
               }
 });
 
