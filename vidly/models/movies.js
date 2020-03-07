f3a0936f6ca5f6203b2be185084197cc31f9a48a
peterkinuthia <peterchege442@gmail.com>
2020-03-07 13:37:34 +0300

    movies routers

--- vidly/models/movies.js
+++ vidly/models/movies.js
@@ -1,6 +1,5 @@
 const Joi = require('joi');
 const mongoose = require ('mongoose');
-const genres = require('./models/genres');
 
 
 const genreSchema = new mongoose.Schema({
---
+++ vidly/routes/movie.js
@@ -0,0 +1,31 @@
+const {Movie, Validate} = require('../models/movies');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router;
+
+
+router.get('/', async(req, res)=>{
+    const movies = await Movie.find().sort('name');
+    res.send(movies);
+
+});
+
+router.post('/', async(req,res)=>{
+    const {error} = Validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    let movie =  new Movie ({
+        title: req.body.title,
+        genre: req.body.genre,
+        numberInstock: req.body.numberInstock,
+        dailyRentalRate: req.body.dailyRentalRate
+    });
+
+    const movie = await movie.save();
+
+    res.send(movie);
+});
+
+
+
+module.exports = router;
