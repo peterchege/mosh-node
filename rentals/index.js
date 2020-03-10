4c390cc34c65474655ad66e6db6ef26b111abf6b
peterkinuthia <peterchege442@gmail.com>
2020-03-10 23:59:41 +0300

    done

--- rentals/index.js
+++ rentals/index.js
@@ -7,3 +7,9 @@ const app = express();
 mongoose.connect('mongodb://localhost/rentals')
     .then(()=> console.log("Connecting to MongoDB..."))
     .catch(err =>console.error('Could not connect to mongoDB...'));
+
+app.use(express.json());
+app.use('api/rentals', rentals);
+
+const port = process.env.port || 3000;
+app.listen(port ()=> console.log(`listening on port`))
