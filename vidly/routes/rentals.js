00c2c23214ca76b5af93abb8259ad4c6377d5919
peterkinuthia <peterchege442@gmail.com>
2020-03-12 17:06:02 +0300

    installation of fawn and init on mongoose for transaction basis

--- vidly/routes/rentals.js
+++ vidly/routes/rentals.js
@@ -2,8 +2,11 @@ const {Rentals, validate} = require('../models/rental');
 const {Movie, validate} = require('../models/movie'); 
 const {Genre} = require('../models/genre');
 const mongoose = require('mongoose');
+const Fawn = require("fawn");
 const express = require('express');
 const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
     const rentals = await Customer.find().sort('-dateOut');
