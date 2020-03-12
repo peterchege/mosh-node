b257696c7094b61a36de8c87abfee57601fc3117
peterkinuthia <peterchege442@gmail.com>
2020-03-12 22:38:03 +0300

    added a email validator

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -7,15 +7,18 @@ const router = express.Router();
 router.post('/', async(req, res)=>{
 
     const {error} = validate(req.body);
-    if(error) return res.status(400).send(error.details[0].message)
+    if(error) return res.status(400).send(error.details[0].message);
 
-    let user = new User({
+    let user = await User.findOne({email: req.body.email});
+    if(user) return res.status(400).send('User already registered.')
+
+    user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
     });
 
-    user = await user.save();
+    await user.save();
 
     res.send(user);
 });
